5831138feb92c5c8e547290623b2b3ad2d93ea8d
yroun <yi@yroun.com>
2021-01-21 16:22:52 +0900

    Update my code

--- morsecode.py
+++ morsecode.py
@@ -54,7 +54,7 @@ def is_help_command(user_input):
     """
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
-    return True if str(user_input).lower() in ['h', 'help'] else False
+    return str(user_input).lower() in ['h', 'help']
     # ==================================
 
 
